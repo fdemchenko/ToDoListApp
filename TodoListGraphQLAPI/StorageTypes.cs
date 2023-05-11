f47ac7e08e5f5fb251a5e5a506306259e26ffc10
Filipp <filipp5632@gmail.com>
2023-05-11 10:42:31 +0300

    Create storage types constants

---
+++ TodoListGraphQLAPI/StorageTypes.cs
@@ -0,0 +1,8 @@
+namespace TodoListGraphQLAPI;
+
+public static class StorageTypes
+{
+    public static string XmlType { get; } = "XML";
+    public static string DatabaseType { get; } = "DATABASE";
+    public static string HeaderName { get; } = "Storage-Type";
+}
