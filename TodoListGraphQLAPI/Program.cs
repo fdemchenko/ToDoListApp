4776f308aed45fe6ceaca3b78eb846097f875e60
Filipp <filipp5632@gmail.com>
2023-05-11 10:43:45 +0300

    Remove default storage, fix usings and add development exception middleware

--- TodoListGraphQLAPI/Program.cs
+++ TodoListGraphQLAPI/Program.cs
@@ -1,10 +1,11 @@
 using GraphQL;
+using TodoListGraphQLAPI;
 using TodoListDAL.Repositories;
 using TodoListDAL.AutoMapperConfig;
 using TodoListGraphQLAPI.Types;
-using TodoListGraphQLAPI.Queries;
-using TodoListGraphQLAPI.Mutations;
 using TodoListGraphQLAPI.TodolistSchema;
+using TodoListGraphQLAPI.GraphQL.Mutations;
+using TodoListGraphQLAPI.GraphQL.Queries;
 
 
 
@@ -22,18 +23,24 @@ builder.Services.AddSingleton<TodoItemRepositoryXML>();
 builder.Services.AddScoped<ICategoryRepository>(provider => {
     HttpContext httpContext = provider.GetRequiredService<IHttpContextAccessor>().HttpContext!;
 
-    httpContext.Request.Headers.TryGetValue("Storage-Type", out var storageType);
-    if (storageType.FirstOrDefault() == "XML")
+    httpContext.Request.Headers.TryGetValue(StorageTypes.HeaderName, out var storageType);
+    if (storageType.FirstOrDefault() == StorageTypes.XmlType)
         return provider.GetRequiredService<CategoryRepositoryXML>();
-    return provider.GetRequiredService<CategoryRepositoryDapper>();
+    else if (storageType.FirstOrDefault() == StorageTypes.DatabaseType)
+        return provider.GetRequiredService<CategoryRepositoryDapper>();
+    else
+        throw new ArgumentOutOfRangeException("Invalid storage type");
 });
 builder.Services.AddScoped<ITodoItemRepository>(provider => {
     HttpContext httpContext = provider.GetRequiredService<IHttpContextAccessor>().HttpContext!;
 
-    httpContext.Request.Headers.TryGetValue("Storage-Type", out var storageType);
-    if (storageType.FirstOrDefault() == "XML")
+    httpContext.Request.Headers.TryGetValue(StorageTypes.HeaderName, out var storageType);
+    if (storageType.FirstOrDefault() == StorageTypes.XmlType)
         return provider.GetRequiredService<TodoItemRepositoryXML>();
-    return provider.GetRequiredService<TodoItemRepositoryDapper>();
+    else if (storageType.FirstOrDefault() == StorageTypes.DatabaseType)
+        return provider.GetRequiredService<TodoItemRepositoryDapper>();
+    else
+        throw new ArgumentOutOfRangeException("Invalid storage type");
 });
 
 builder.Services.AddSingleton<CategoryType>();
@@ -48,7 +55,10 @@ builder.Services.AddGraphQL(builder => builder
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 app.UseGraphQL();
 app.UseGraphQLPlayground();
 
